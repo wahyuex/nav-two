b120421c418cec2cc37aae5ac38799199f2a29c1
wahyuex <88044555+wahyuex@users.noreply.github.com>
2023-10-22 09:29:21 +0700

    ppush modul 5

---
+++ screens/index.js
@@ -0,0 +1,29 @@
+import LotsOfStyles from "./LotsOfStyles";
+import FixedDimensionsBasics from "./FixedDimensionsBasics";
+import FlexDimensionsBasics from "./FlexDimensionsBasics";
+import PercentageDimensionsBasics from "./PercentageDimensionBasics";
+import FlexBasic from "./FlexBasic";
+import FlexDirectionBasics from "./FlexDirectionBasics.js";
+import JustifyContentBasics from "./JustifyContentBasics.js";
+import AlignItemsLayout from "./AlignItemsLayout.js";
+import WidthHeightBasics from "./WidthHeightBasics.js";
+import PositionLayout from "./PositionLayout.js";
+import DisplayAnImageWithStyle from "./DisplayAnImageWithStyle";
+import NativeBaseBasic from "./NativeBaseBasic.js";
+import GlueStackUIBasic from "./GlueStackUIBasic.js";
+
+export {
+  LotsOfStyles,
+  FixedDimensionsBasics,
+  FlexDimensionsBasics,
+  PercentageDimensionsBasics,
+  FlexBasic,
+  FlexDirectionBasics,
+  JustifyContentBasics,
+  AlignItemsLayout,
+  WidthHeightBasics,
+  PositionLayout,
+  DisplayAnImageWithStyle,
+  NativeBaseBasic,
+  GlueStackUIBasic
+}
